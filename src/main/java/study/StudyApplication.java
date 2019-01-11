bc59cf4c32ead8ede198718b613c410df85cfc27
Leo Luo <robzai@hotmail.com>
2019-01-10 16:23:49 -0800

    tried type save configuration & configuration with profiles

--- src/main/java/study/StudyApplication.java
+++ src/main/java/study/StudyApplication.java
@@ -5,24 +5,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import study.Beans.MyBean;
+import study.config.TypeSafeConfiguration;
 
 
 //@EnableAutoConfiguration
 @SpringBootApplication    // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@RestController    // tells our class is a web @Controller, so Spring considers it when handling incoming web requests. + 
-                   // @RestController annotation tells Spring to render the resulting string directly back to the caller.
+@RestController           // tells our class is a web @Controller, so Spring considers it when handling incoming web requests. + 
+                          // @RestController annotation tells Spring to render the resulting string directly back to the caller.
+@EnableConfigurationProperties({
+								TypeSafeConfiguration.class
+							  })    // When using configuration classes, you also need to list the properties
+                                    // classes to register in the @EnableConfigurationProperties annotation
 public class StudyApplication {
 	
 	@Autowired
 	MyBean mybean;
 	
+	@Autowired
+	TypeSafeConfiguration typeSafeConfiguration;
+	
 	@RequestMapping("/")    // our class is a web @Controller, so Spring considers it when handling incoming web requests.
 	String home() {		
-		return "Hello " + mybean.name;
+//		return "Hello " + mybean.name;
+		return "typesave config: " + typeSafeConfiguration.getSecurity().getUsername();
 	}
 
 	public static void main(String[] args) {
--- src/main/java/study/config/TypeSafeConfiguration.java
+++ src/main/java/study/config/TypeSafeConfiguration.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("acme")
+@ConfigurationProperties("acme")    // bind YAML to properties using Spring Boot’s Binder utilities 
 public class TypeSafeConfiguration {
 
 	private boolean enabled;
@@ -17,10 +17,40 @@ public class TypeSafeConfiguration {
 
 		private String username;
 
-		private String password;
+//		private String password;
 
 		private List<String> roles = new ArrayList<>(Collections.singleton("USER"));
 
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public List<String> getRoles() {
+			return roles;
+		}
+
+		public void setRoles(List<String> roles) {
+			this.roles = roles;
+		}
+
 	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Security getSecurity() {
+		return security;
+	}
+
+	
 	
 }
